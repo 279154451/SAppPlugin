af2b6ab33d61f91acea366266d7e4064aa237b51
CZF <279154451@qq.com>
2022-03-02 21:16:43 +0800

    适配AppCompatActivity,解决插件获取资源空指针问题

--- pluginapp/src/main/java/com/single/code/app/pluginapp/BaseActivity.java
+++ pluginapp/src/main/java/com/single/code/app/pluginapp/BaseActivity.java
@@ -1,19 +1,50 @@
 package com.single.code.app.pluginapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.single.code.app.pluginlib.PluginUtils;
+
+import java.lang.reflect.Field;
 
 /**
  * 创建时间：2022/3/1
  * 创建人：singleCode
  * 功能描述：
  **/
-public class BaseActivity extends Activity {
+public class BaseActivity extends AppCompatActivity {
+    protected Context mContext;
     @Override
-    public Resources getResources() {
-        Resources resources = PluginUtils.getResources(getApplication());
-        return resources == null ? super.getResources() : resources;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Resources resource = PluginUtils.getResources(getApplication());
+        if(resource != null){
+            // 替换这个 context 的resource 为我们自己写的 resource，外面通过mContext获取到的就是插件的资源了。这样能够解决插件的资源与宿主资源冲突
+            mContext = new ContextThemeWrapper(getBaseContext(), 0);
+
+            Class<? extends Context> clazz = mContext.getClass();
+            try {
+                Field mResourcesField = clazz.getDeclaredField("mResources");
+                mResourcesField.setAccessible(true);
+                mResourcesField.set(mContext, resource);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }else {
+            mContext = this;
+        }
     }
+//    @Override
+//    public Resources getResources() {
+//        Resources resources = PluginUtils.getResources(getApplication());
+//        return resources == null ? super.getResources() : resources;//这样方式的缺点时无法解决插件与宿主资源的冲突
+//    }
 }
--- pluginapp/src/main/java/com/single/code/app/pluginapp/MainActivity.java
+++ pluginapp/src/main/java/com/single/code/app/pluginapp/MainActivity.java
@@ -2,14 +2,17 @@ package com.single.code.app.pluginapp;
 
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.activity_main,null);//这里需要使用新创建的Context来获取插件资源
+        setContentView(view);
     }
 
 }
